8c61232b7f2bcff884a509b8b8d03945cc4a5c9a
¨MatyAntunez¨ <matias.antunez410@gmail.com>
2023-11-29 16:26:49 -0300

    darkTheme

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import { NextUIProvider } from '@nextui-org/react'
 import '@/styles/globals.css'
+import { darkTheme } from '@/themes'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <main className="dark text-foreground bg-background">
+        < Component {...pageProps} />
+      </main>
+    </NextUIProvider>
+  )
 }
